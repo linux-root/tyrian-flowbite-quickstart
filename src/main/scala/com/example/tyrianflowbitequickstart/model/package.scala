0ad00f21d667bc15b4fb99e7b753304cdfbf36cc
Watson Dinh <bao.dinh@smarterhealth.sg>
2025-01-17 20:26:04 -0600

    update with dark mode

--- src/main/scala/com/example/tyrianflowbitequickstart/model/package.scala
+++ src/main/scala/com/example/tyrianflowbitequickstart/model/package.scala
@@ -4,17 +4,21 @@ import tyrian.Location
 
 object model {
   enum Msg {
-    case NoOp                    extends Msg
-    case LogMessage(msg: String) extends Msg
+    case NoOp
+    case LogMessage(msg: String)
     case NavigateTo(page: Page)
     case DoNavigate(page: Page)
     case UnhandledRoute(path: String)
     case GoToInternet(loc: Location.External)
+    case ToggleDarkMode
   }
 
-  case class Model(currentPage: Page)
+  case class Model(currentPage: Page, isDarkMode: Boolean) {
+    def toggleDarkMode: Model =
+      copy(isDarkMode = !isDarkMode)
+  }
 
   object Model {
-    val init: Model = Model(Page.Home)
+    val init: Model = Model(Page.Home, isDarkMode = false)
   }
 }
