eef136d02812aeda01baf6aa93e9141f1493acfc
Watson Dinh <mr.kurro@gmail.com>
2025-02-09 10:12:23 -0600

    update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,6 @@ object Dependencies {
   val Tyrian    = "0.12.0"
   val Scalatest = "3.2.15"
   val Quicklens = "1.9.7"
-
-  val ZioInteropCats = "23.1.0.3"
   val JavaSecureRandom = "1.0.0"
+  val ZioInteropCats = "23.1.0.3"
 }
